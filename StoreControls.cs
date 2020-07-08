1377f6bae76f0b8e206bc64c6c0a3117fc5834ce
Bryan <thomas.bryan.m@gmail.com>
2020-07-07 21:20:24 -0700

    add Controls Class

---
+++ StoreControls.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OpenQA.Selenium;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System.Collections.Generic;
+
+namespace QA_BandWebsite
+{
+    public class StoreControls
+    {
+        #region Controls
+        #endregion
+
+        #region Methods
+        #endregion
+
+    }
+}
